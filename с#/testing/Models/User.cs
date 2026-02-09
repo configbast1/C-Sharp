9a9256efd21b1c0bb3d04570600c8729bfb3fda7
configbast1 <stiilboimailur@gmail.com>
2026-02-09 18:08:31 +0100

    Create User.cs

---
+++ с#/testing/Models/User.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace testing.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = ""; 
+        public string Password { get; set; } = ""; 
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
